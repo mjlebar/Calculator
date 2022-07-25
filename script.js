73bb1d38f92d3a80044ff030c9260989cb7c1570
mjlebar <106131602+mjlebar@users.noreply.github.com>
2022-07-25 17:07:16 -0500

    Complete JS code

--- script.js
+++ script.js
@@ -21,10 +21,27 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  return operator(a, b);
+  let ans;
+  if (operator === "+") ans = a + b;
+  else if (operator === "-") ans = a - b;
+  else if (operator === "*") ans = a * b;
+  else if (operator === "÷") {
+    if (b === 0) {
+      return "ERROR!";
+    } else {
+      ans = a / b;
+    }
+  }
+  if (ans.toString().length > 30) {
+    beforeDec = ans.toString().split(".")[0].length;
+    return ans.toFixed(26 - beforeDec);
+  }
+  return ans;
 }
 
 let displayVal;
+let calcVal;
+let operator;
 
 buttons.forEach((button) => button.addEventListener("click", updateDisplay));
 
@@ -32,12 +49,40 @@ function updateDisplay(e) {
   const btn = e.target.textContent;
   if (btn === "C") {
     display.textContent = "";
-  } else if (btn === "=") {
-  } else {
-    display.textContent += btn;
+    operator = null;
+    calcVal = null;
+    displayVal = null;
+  } else if (
+    btn === "=" &&
+    operator &&
+    Number.isFinite(calcVal) &&
+    Number.isFinite(displayVal)
+  ) {
+    displayVal = operate(operator, calcVal, displayVal);
+    display.textContent = `${displayVal}`;
+    operator = null;
+    calcVal = null;
+  } else if (btn != "=") {
     if (btn === "+" || btn === "*" || btn === "-" || btn === "÷") {
+      if (operator) {
+        displayVal = operate(operator, calcVal, displayVal);
+        display.textContent = `${displayVal}`;
+        operator = btn;
+        calcVal = displayVal;
+      } else {
+        calcVal = displayVal;
+        operator = btn;
+        displayVal = null;
+        display.textContent = "";
+      }
     } else {
-      displayVal = Number(display.textContent);
+      if (operator) {
+        displayVal = Number(btn);
+        display.textContent = displayVal;
+      } else {
+        display.textContent += btn;
+        displayVal = Number(display.textContent);
+      }
     }
   }
 }
